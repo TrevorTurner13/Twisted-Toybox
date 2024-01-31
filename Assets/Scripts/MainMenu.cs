0897b3c723c0c16f0a7fb42958a1cc85ea727a69
quinkennett <quinton.kennett@easternstudent.ca>
2024-01-31 16:49:37 -0400

    Added SFX to main menu and made changed in Jester level

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,19 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI; // Required for working with UI
+
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private AudioClip startGameSound;
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(3);
+        SoundFXManager.instance.PlaySoundFXClip(startGameSound, transform, 1f);
+
+        StartCoroutine(LoadSceneCoroutine());
     }
 
-    public void ControlsMenu()
+    IEnumerator LoadSceneCoroutine()
     {
+        yield return new WaitForSeconds(3.25f);
         SceneManager.LoadScene(2);
     }
+
+    //public void PlayGame()
+    //{
+
+    //    SceneManager.LoadScene(2);
+
+    //}
 
     public void QuitGame()
     {
@@ -25,4 +39,5 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
 }
