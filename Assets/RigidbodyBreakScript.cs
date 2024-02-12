9b26af202e569f294316e5259ad6116c661d0dd2
TrevorTurner13 <118460902+TrevorTurner13@users.noreply.github.com>
2024-02-12 14:08:47 -0400

    added rickety platform, and elevator sprites

--- Assets/RigidbodyBreakScript.cs
+++ Assets/RigidbodyBreakScript.cs
@@ -6,18 +6,29 @@ public class RigidbodyBreakScript : MonoBehaviour
 {
 
     List<Rigidbody2D> rigidbodies = new List<Rigidbody2D>();
+    List<Transform> childTransforms = new List<Transform>();
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbodies.Clear();
+        childTransforms.Clear();
 
         for (int i = 0; i < transform.childCount; i++)
         {
+            
             Rigidbody2D rigidbody = transform.GetChild(i).GetComponent<Rigidbody2D>();
 
             if (rigidbody != null)
             {
                 rigidbodies.Add(rigidbody);
+            }
+
+            Transform childTransform = transform.GetChild(i).transform;
+
+            if (childTransform != null)
+            {
+                childTransforms.Add(childTransform);
             }
         }
     }
@@ -29,8 +40,8 @@ public class RigidbodyBreakScript : MonoBehaviour
             foreach (var rigidbody in rigidbodies)
             {
                 rigidbody.isKinematic = false;
+                
             }
         }
     }
-
 }
